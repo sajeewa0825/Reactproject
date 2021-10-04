86ee4d62e9082c1bb457167f40873703103af89b
Lashen Kariyawasam <56374597+LashenKariyawasam@users.noreply.github.com>
2021-10-04 13:09:13 +0530

    Change the Home Component and fix the view of the site

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import {Container, H1} from "./HomeStyles";
+
+const Home = () => {
+    return (
+        <Container>
+            <H1>Alone</H1>
+        </Container>
+    );
+};
+export default Home;
